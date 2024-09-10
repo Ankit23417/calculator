7ed6b4588e4eddd87173200f27d905b17a37634e
hp <ankitjangra648@gmail.com>
2024-09-10 19:16:48 +0530

    commit calculator

---
+++ main.py
@@ -0,0 +1,17 @@
+a= int(input(" enter the first value:"))
+b= int(input(" enter the second value:"))
+
+add = a+b
+print("first value + second value:",add)
+
+sub = a-b
+print("first value - second value:",sub)
+
+mul = a*b
+print("first value * second value:",mul)
+
+div = a/b
+print("first value /  second value:",div)
+
+mod = a%b
+print("first value % second value:",mod)
